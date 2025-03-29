f6ff2f10270a30f26dd53bfea34a9905a8a50863
Sefa <sefa.beser@stud.hs-hannover.de>
2025-03-29 22:03:33 +0100

    feat: upload Files with Postman or curl

--- src/main/java/com/Projekt/RaspberryCloud/service/DataService.java
+++ src/main/java/com/Projekt/RaspberryCloud/service/DataService.java
@@ -1,10 +1,14 @@
 package com.Projekt.RaspberryCloud.service;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.Projekt.RaspberryCloud.model.Data;
 import com.Projekt.RaspberryCloud.repository.DataRepository;
@@ -41,6 +45,23 @@ public class DataService {
     public String addData(Data data) {
         dataRepository.save(data);
         return "Speichern erfolgreich";
+    }
+
+    // ToDO: Es wird nicht gegengecheckt ob die Hochzuladende Datei bereits
+    // existiert
+    public String upload(MultipartFile file) {
+        Data newData;
+        try {
+            byte[] fileInput = file.getBytes();
+            Path filePath = Paths.get("src/main/resources/uploads", file.getOriginalFilename());
+            Files.write(filePath, fileInput);
+            newData = new Data(null, "pdf", file.getOriginalFilename(), filePath.getParent().toString());
+            dataRepository.save(newData);
+        } catch (Exception e) {
+            System.out.println("Fehler beim Einlesen der Datei");
+        }
+
+        return "Datei gespeichert";
     }
 
 }
